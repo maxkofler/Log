bf0df4505f9fafda3f9aa689995ed89f90561fe7
Max Kofler <kofler.max.dev@gmail.com>
2021-10-18 09:15:27 +0200

    Moved mutex check to improve performance

--- src/log/output.cpp
+++ src/log/output.cpp
@@ -8,21 +8,24 @@
 
 namespace Log{
     bool Log::log(level loglevel, std::string function, std::string message){
-        #ifndef LOG_NOMUTEX
-        this->_m_logging.lock();
-        #else
-            #warning DISABLED LOG MUTEX, THIS WILL IMPROVE PERFORMANCE! this can lead to overlapping log messages!
-        #endif
         if (loglevel <= this->_loglevel){
+            #ifndef LOG_NOMUTEX
+            this->_m_logging.lock();
+            #else
+                #warning DISABLED LOG MUTEX, THIS WILL IMPROVE PERFORMANCE! this can lead to overlapping log messages!
+            #endif
+
+
             if (this->_print_function_names){
                 std::cout << "[" << function << "]>>>" << message << std::endl;
             }else{
                 std::cout << message << std::endl;
             }
+
+            #ifndef LOG_NOMUTEX
+            this->_m_logging.unlock();
+            #endif
         }
-        #ifndef LOG_NOMUTEX
-        this->_m_logging.unlock();
-        #endif
         return true;
     }
 
