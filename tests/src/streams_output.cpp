f71a007ec8f3ff6a349c2abf2b9e29a4402c57c2
Max Kofler <kofler.max.dev@gmail.com>
2022-11-23 11:21:15 +0100

    Added tests for new streams

---
+++ tests/src/streams_output.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file		tests/streams_output.cpp
+ * @author		Max Kofler (kofler.max.dev@gmail.com)
+ * @brief		The test for output to streams
+ * @copyright	Copyright (c) 2022
+ */
+
+#include <gtest/gtest.h>
+
+#include <sstream>
+
+#include "log.h"
+
+TEST(Log, Streams_Output){
+	Log::Log l(Log::D);
+
+	std::stringstream ss;
+
+	Log::stream_config c;
+	c.loglevel = Log::D;
+	c.print_function_names = false;
+
+	l.addStream(ss, c);
+
+	std::string testStr = "TestString";
+
+	l.log(Log::D, "TestFun", testStr);
+
+	ASSERT_EQ(testStr + "\n", ss.str());
+}
