e81e1e1ab321c2eb9e83d32a4d68f9adc6d00f04
Max Kofler <kofler.max.dev@gmail.com>
2022-11-23 12:05:29 +0100

    Added tests for getStreamConf()

--- tests/src/streams_output.cpp
+++ tests/src/streams_output.cpp
@@ -26,8 +26,16 @@ TEST(Log, Streams_Output){
 
 	l.log(Log::D, "TestFun", testStr);
 
-	ASSERT_EQ(testStr + "\n", ss.str());
+	ASSERT_EQ(testStr + "\n", ss.str()) << "Did not log the string to the sstream";
 
 	l.log(Log::MEM, "TestFun", testStr);
-	ASSERT_EQ(testStr + "\n", ss.str());
+	ASSERT_EQ(testStr + "\n", ss.str()) << "Accidentally logged wrong loglevel to sstream";
+
+	Log::stream_config* cNew = l.getStreamConf(ss);
+	ASSERT_NE(nullptr, cNew) << "Could not find associated stream!";
+
+	cNew->loglevel = Log::MEM;
+
+	l.log(Log::MEM, "TestFun", testStr);
+	ASSERT_EQ(testStr + "\n" + testStr + "\n", ss.str()) << "Loglevel did not change";
 }
